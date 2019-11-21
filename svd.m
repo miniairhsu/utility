3f93e67d963f5e5ccbeccbf442288e4b5cd85a19
Len Hsu <miniair@hotmail.com>
2019-11-21 21:16:00 +0800

    1. add SVD vs eigen vector comparison

--- svd.m
+++ svd.m
@@ -1,21 +1,21 @@
 clear
 
 #plot vector from origin 
-function plotVectors(vecs)
+function plotVectors(vecs, color)
     v = vecs'
     for i = 1 : length(v) #length is num of rows
         x = cat(1, [0,0], v(i,:));
         quiver(x(1),  #start x
                x(3),  #start y
                x(2),  #x direction
-               x(4),'Linewidth',6); #y direction
+               x(4),'Linewidth',4, color); #y direction
         grid on 
         hold on
     end
 end 
 
 #transform a unit vector by applying a matrix
-function matrixToPlot(matrix)
+function matrixToPlot(matrix, color)
     # Unit circle
     x = linspace(-1, 1, 100000);
     y = sqrt(1-(x.^2));
@@ -30,7 +30,7 @@ function matrixToPlot(matrix)
     u = [matrix(1,1),matrix(1,2)];
     v = [matrix(2,1),matrix(2,2)];
 
-    plotVectors([u; v]);
+    plotVectors([u; v], color);
 
     plot(x1, y1, 'b','Linewidth',6 ,x1_neg, y1_neg, 'b','Linewidth',6);
     grid on 
@@ -59,26 +59,34 @@ title('Transformed circle');
 v = [1 0];
 u = [0 2];
 figure(2)
-plotVectors([v; u])
+plotVectors([v; u], 'b')
 
 #apply matrix transform to vector 
 v1 = [3 7];
 u1 = [5 2];
 A = [v1; u1];
 figure(3)
-matrixToPlot(A);
+matrixToPlot(A, 'g');
 
 #apply SVD decomposition
-[U,S,V] = svd(A);
+[U,S,V] = svd(A); #vector is columns of matrix
 figure(4) 
-matrixToPlot(V'); #apply rotation compoenent to unit vector
+matrixToPlot(V', 'g'); #apply rotation compoenent to unit vector
 
 figure(5)
-matrixToPlot(S*(V')); #apply scaling to unit vector 
+matrixToPlot(S*(V'), 'g'); #apply scaling to unit vector 
 
 figure(6)
-matrixToPlot((U')*S*(V')); #apply scaling to unit vector 
+matrixToPlot((U')*S*(V'), 'g'); #apply rotation to unit vector 
 
+u1 = [S(1)*U(1,1), S(4)*U(2,1)]; 
+v1 = [S(1)*U(1,2), S(4)*U(2,2)];
+plotVectors([u1; v1], 'r');
 
+A1 = [7 2;3 4;5 3];
+[U1,S1,V1] = svd(A1);
 
+[VA,DA] = eig((A1')*A1); 
+[VA1,DA1] = eig(A1*(A1')); #VA = V1  
+sqrtDA = sqrt(DA1)
 
