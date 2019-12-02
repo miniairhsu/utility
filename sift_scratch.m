5b35659d31ad7c29124c998a25c6ecf8da59b07a
Len <miniair@hotmail.com>
2019-12-02 21:47:51 +0800

    1. add local maxima and minima

--- sift_scratch.m
+++ sift_scratch.m
@@ -2,9 +2,9 @@ clear all
  
 function octaves = gen_Octave(img,s, sigma)
   k = 2^(1.0/s);
-  kernel = fspecial('gaussian',max(1,fix(6*sigma)), k*sigma);
   octaves(:,:,1) = img;
   for i = 1: (s+2)
+    kernel = fspecial('log',[5 5], (k*(2^(i-1)))*sigma); #laplacian of gaussian
     octaves(:,:,i+1) = conv2(octaves(:,:,i), kernel, 'same');
   end 
 end  
@@ -12,14 +12,14 @@ end
 function octavesD = gen_DOG(octaves)
   n = size(octaves,3); #no of octaves
   for i = 2:n
-    octavesD(:,:,i-1) = octaves(:,:,i) - octaves(:,:,i-1);
+    octavesD(:,:,i-1) = octaves(:,:,i-1) - octaves(:,:,i);
   end
 end
 
 function pyramid = gen_pyramid(img, level, s, sigma)
   pyramid = {};
   for i = 1:level #number of scales
-    octaves = gen_Octave(imresize(img, 0.5^(i)),s, sigma);
+    octaves = gen_Octave(imresize(img, 0.5^(i-1)),s, sigma);
     octavesD = gen_DOG(octaves);
     for j = 1:size(octavesD,3)
       pyramid(i,j) = octavesD(:,:,j);
@@ -29,29 +29,49 @@ function pyramid = gen_pyramid(img, level, s, sigma)
   end
 end
 
-function extrama = find_MinMax(pyramid, w) #w is kernel size / 2 
-  
+function extrama = find_MinMax(pyramid) #w is kernel size / 2 
+  extrama = {};
   for i = 1:size(pyramid,1) #number of scales
+    local_extrama = zeros(size(cell2mat(pyramid(i,1)),1), size(cell2mat(pyramid(i,1)),2), size(pyramid,2)-2);
     for j = 2:size(pyramid,2)-1 #number of octaves 
-      previous = cell2mat(pyramid(i,j-1);
-      current = cell2mat(pyramid(i,j);
-      next = cell2mat(pyramid(i,j+1);
-      for row = 3 : size(current,1) - 3 
-        for col = 3 : size(current,2) - 3
+      previous = cell2mat(pyramid(i,j-1));
+      current = cell2mat(pyramid(i,j));
+      next = cell2mat(pyramid(i,j+1));
+      for row = 2 : size(current,1) - 2 
+        for col = 2 : size(current,2) - 2
+          local_extramaP = previous(row-1:row+1, col-1:col+1);
+          local_extramaC = current(row-1:row+1, col-1:col+1);
+          local_extramaN = next(row-1:row+1, col-1:col+1);
+          [M2,I2] = max(local_extramaC(:));
+          if(I2 == 5)
+            if(M2 > max(local_extramaP(:)) && M2 > max(local_extramaN(:)))
+              local_extrama(row, col, j-1) = 1;
+            end 
+          end 
+          [M1,I1] = min(local_extramaC(:));
+          if(I1 == 5)
+            if(M1 < min(local_extramaP(:)) && M1 < min(local_extramaN(:)))
+              local_extrama(row, col, j-1) = 1;
+            end 
+           end 
         end 
+       end 
+       extrama(i,j-1) = local_extrama(:,:, j-1);
       end 
-  end 
-end
+    end
+end 
 
 
 img=imread('operahouse.jpg');
 imgGray=rgb2gray(img);
 imgD = double(imgGray);
-pyramid = gen_pyramid(imgD, 4, 5, 1.6); #4 scales, 5 octaves
-#octaves = gen_Octave(imgD,6,3);
+pyramid = gen_pyramid(imgD, 4, 5, 0.707); #4 scales, 5 octaves
+#octaves = gen_Octave(imgD,5,0.707);
 #octavesD = gen_DOG(octaves);
 #pry1 = octavesD;
-
+#octave1 = octavesD(:,:,1);
+#octave2 = octavesD(:,:,2);
+#octave3 = octavesD(:,:,3);
 #octaves2  = gen_Octave(imresize(imgD,0.5), 5, 0.6);
 #octavesD2 = gen_DOG(octaves2);
 #pry2 = octavesD2;
@@ -63,20 +83,20 @@ pyramid = gen_pyramid(imgD, 4, 5, 1.6); #4 scales, 5 octaves
 #octaves4  = gen_Octave(imresize(imgD,0.125), 5, 0.6);
 #octavesD4 = gen_DOG(octaves4);
 #pry4 = octavesD4;
-[local_maxima, local_minima] = find_MinMax(pyramid);
+local_maxima = find_MinMax(pyramid);
+minMax = cell2mat(local_maxima(1,4));
   
 figure(1)
-pry1 = cell2mat(pyramid(2,1));
-imshow(uint8(pry1));
+pry1 = cell2mat(pyramid(1,1));
+imshow(uint8(minMax)*255);
 figure(2)
-pry1 = cell2mat(pyramid(2,2));
 imshow(uint8(pry1));
-figure(3)
-pry2 = cell2mat(pyramid(2,3));
-imshow(uint8(pry2));
-figure(4)
-pry3 = cell2mat(pyramid(2,4));
-imshow(uint8(pry3));
+#figure(3)
+#pry2 = cell2mat(pyramid(2,3));
+#imshow(uint8(pry2));
+#figure(4)
+#pry3 = cell2mat(pyramid(2,4));
+#imshow(uint8(pry3));
 
 
 
